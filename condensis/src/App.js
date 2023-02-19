34265f332719408ed34baad6e23f1acf00cd316f
Aaryan0404 <48185825+Aaryan0404@users.noreply.github.com>
2023-02-19 04:37:44 -0800

    Add files via upload

--- condensis/src/App.js
+++ condensis/src/App.js
@@ -1,32 +1,41 @@
 import Navbar from "./components/Navbar";
 import "./Styles/App.css";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, 
+  Route} from "react-router-dom";
+import About from './pages/About';
 import Courses from './pages/Courses';
 import Home from "./pages/Home";
-import CourseDetail from "./pages/CourseDetail";
-import About from "./pages/About";
 import StanfordCourses from "./pages/StanfordCourses";
+import CreateInvoice from "./pages/Invoice";
+
+import { createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Sora',
+      'sans-serif',
+    ].join(','),
+  },});
+
+
 export default function App() {
+
   return (
     <div className="App">
-
+      <ThemeProvider theme={theme}>
       <Router>
+        
         <Navbar/>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="/Courses" element={<Courses/>}/>
           <Route exact path="/About" element={<About/>}/>
           <Route exact path="/Stanford" element={<StanfordCourses/>}/>
-          <Route exact path="/course_detail/0" element={<CourseDetail course_id={0} />}/>
-
-          <Route exact path="/course_detail/1" element={<CourseDetail course_id={1} />}/>
-          <Route exact path="/course_detail/2" element={<CourseDetail course_id={2} />}/>
-
+          <Route exact path="/Invoice" element={<CreateInvoice/>}/>
         </Routes>
-
-            {/*<Route  exact path="/course_detail/:id" element={<CourseDetail />}/>*/}
-
       </Router>
+      </ThemeProvider>
     </div>
     
   );
