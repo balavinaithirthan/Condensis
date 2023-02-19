9f967c828cc08bfbcc77826113e80e29542992a4
AaryanSinghal <aaryan0404@gmail.com>
2023-02-19 08:17:23 -0800

    jygy

--- video_segmentation.py
+++ video_segmentation.py
@@ -2,8 +2,11 @@
 import cv2
 import numpy as np
 import datetime
+import matplotlib.pyplot as plt
  
 arr = []
+arrx = []
+arry = []
 
 #################### Setting up the file ################
 
@@ -20,19 +23,18 @@ frame_counter += 1
 #################### Setting up parameters ################
 frames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
 fps = vidcap.get(cv2.CAP_PROP_FPS)
-  
-# calculate duration of the video
-seconds = round(frames / fps)
-video_time = datetime.timedelta(seconds=seconds)
+
 
 #################### Initiate Process ################
 
 while (success_prev & success_next):
     dest_xor = cv2.bitwise_xor(image_prev, image_next, mask = None)
-    number_of_white_pix = np.sum(dest_xor == 255)
-    cv2.imshow('Bitwise XOR', dest_xor)
-    if (number_of_white_pix > 100): # random threshold
+    number_of_total_pix = np.sum(dest_xor >= 0)
+    number_of_changed_pix = np.sum(dest_xor > 0)
+    if (number_of_changed_pix/number_of_total_pix > 0.57): # random threshold
         arr.append(frame_counter * (1/fps))
+    arrx.append(frame_counter * (1/fps))
+    arry.append(number_of_changed_pix/number_of_total_pix)
 
     success_prev, image_prev = vidcap.read()
     frame_counter += 1
@@ -42,6 +44,14 @@ while (success_prev & success_next):
         
 vidcap.release()
 print("Complete")
+
+plt.plot(arrx, arry)
+
+plt.xlabel('x: seconds in video')
+plt.ylabel('y: percent of pixels changed')
+
+plt.title('Slide Change Detection')
+plt.show()
  
 # Closes all the windows currently opened.
 cv2.destroyAllWindows()
