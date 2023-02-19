c52154b377da4e98356f922e773771912600c2d4
Arjun Vikram <arjvik@gmail.com>
2023-02-19 08:39:12 -0800

    Fix titles

--- condensis/src/components/LectureCard.js
+++ condensis/src/components/LectureCard.js
@@ -2,47 +2,47 @@ import React, {Component} from 'react';
 import { CCard, CButton, CCardBody, CCardFooter, CCardGroup, CCardHeader, CCardImage, CCardLink, CCardSubtitle, CCardText, CCardTitle } from '@coreui/react'
 import '../Styles/LectureCard.css'
 var lectures = [{
-    "Title": "Lecture 1: Introductions, Counting",
+    "Title": "Lecture 1: Getting Started with C",
     "Date": "Jan 9, 2023",
     "Preview": "Welcome to CS 106B",
     "notes_id": 0
 }, {
-    "Title": "Lecture 2: Introductions, Counting",
+    "Title": "Lecture 2: Getting Started with Recursion",
     "Date": "Jan 11, 2023",
     "Preview": "Welcome to CS 106B",
     "notes_id": 1
 }, {
-    "Title": "Lecture 3: Introductions, Counting",
+    "Title": "Lecture 3: Collections Part I",
     "Date": "Jan 13, 2023",
     "Preview": "Welcome to CS 106B",
     "notes_id": 2
 }, {
-    "Title": "Lecture 4: Introductions, Counting",
+    "Title": "Lecture 4: Collections Part II",
     "Date": "Jan 15, 2023",
     "Preview": "Welcome to CS 106B", 
     "notes_id": 3
 }, {
-    "Title": "Lecture 5: Introductions, Counting",
+    "Title": "Lecture 5: Collections Part III",
     "Date": "Jan 17, 2023",
     "Preview": "Welcome to CS 106B", 
     "notes_id": 4
 }, {
-    "Title": "Lecture 6: Introductions, Counting",
+    "Title": "Lecture 6: Thinking Recursively Part I",
     "Date": "Jan 19, 2023",
     "Preview": "Welcome to CS 106B", 
     "notes_id": 5
 }, {
-    "Title": "Lecture 7: Introductions, Counting",
+    "Title": "Lecture 7: Thinking Recursively Part II",
     "Date": "Jan 21, 2023",
     "Preview": "Welcome to CS 106B", 
     "notes_id": 6
 }, {
-    "Title": "Lecture 8: Introductions, Counting",
+    "Title": "Lecture 8: Thinking Recursively Part III",
     "Date": "Jan 23, 2023",
     "Preview": "Welcome to CS 106B", 
     "notes_id": 7
 }, {
-    "Title": "Lecture 9: Introductions, Counting",
+    "Title": "Lecture 9: Thinking Recursively Part IV",
     "Date": "Jan 25, 2023",
     "Preview": "Welcome to CS 106B", 
     "notes_id": 8
